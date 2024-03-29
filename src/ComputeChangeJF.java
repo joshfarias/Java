075d5b8a6b7861fbea5999cd335919b7a24c1419
Josh Farias <joshfarias.github@gmail.com>
2024-03-29 18:40:15 -0400

    computes change
    computes change

---
+++ src/ComputeChangeJF.java
@@ -0,0 +1,42 @@
+/******************************************
+* @Author Joshua Farias
+* @since 2020.09.08
+* Program: ComputeChangeJF.java
+* Purpose: Compute change
+*******************************************/
+                                      
+import java.util.Scanner;
+
+public class ComputeChangeJF {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+      
+      System.out.print( "Enter an amount in double, for example 11.56: ");
+       		double amount = input.nextDouble();
+            
+      int remainingAmount = (int)(amount * 100);
+      
+      int numberOfOneDollars = remainingAmount / 100;
+      remainingAmount = remainingAmount % 100;
+      
+      int numberOfQuarters = remainingAmount / 25;
+      remainingAmount = remainingAmount % 25;
+      
+      int numberOfDimes = remainingAmount / 10;
+      remainingAmount = remainingAmount % 10;
+      
+      int numberOfNickels = remainingAmount / 5;
+      remainingAmount = remainingAmount % 5;
+      
+      int numberOfPennies = remainingAmount;
+      
+      System.out.println("Your amount " + amount + " consists of");
+      System.out.println(" " + numberOfOneDollars + " dollars");
+      System.out.println(" " + numberOfQuarters + " quarters ");
+      System.out.println(" " + numberOfDimes + " dimes");
+      System.out.println(" " + numberOfNickels + " nickels");
+      System.out.println(" " + numberOfPennies + " pennies");
+      
+     }
+     
+ }
